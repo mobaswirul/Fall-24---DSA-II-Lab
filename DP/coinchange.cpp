0a2bfd70a3900a12233cda4c250f147e89f4967b
Mobaswirul Islam <mobaswirul@cse.uiu.ac.bd>
2025-01-02 06:18:13 +0000

    dp start

--- DP/coinchange.cpp
+++ DP/coinchange.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int path[1000];
+
 int coinChange(int amount, vector<int>& coins, vector<int>& dp){
     if(amount == 0) return 0;
     if(dp[amount] != -1) return dp[amount];
@@ -9,15 +11,30 @@ int coinChange(int amount, vector<int>& coins, vector<int>& dp){
     for(int i=0; i<n; i++){
         if(coins[i] == amount) return 1;
         if(coins[i] > amount) continue;
-        ans = min(ans, 1 + coinChange(amount - coins[i], coins, dp));
+
+        int retAns = 1 + coinChange(amount - coins[i], coins, dp);
+
+        if(retAns < ans){
+            ans = retAns;
+            path[amount] = amount - coins[i];
+        } 
     }
     return dp[amount] = ans;
 }
 
 int main(){
-    int amount = 300;
+    int amount = 31;
     vector<int> dp(amount + 1, -1);
     vector<int> coins = {25, 10, 1};
     sort(coins.begin(), coins.end(), greater<int>());
     cout << coinChange(amount, coins, dp) << endl;
+
+    // for(int i=0; i<=amount; i++){
+    //     cout << i << " " << path[i] << endl;
+    // }
+    int cr = amount;
+    while(cr > 0){
+        cout << cr - path[cr] << " ";
+        cr = path[cr];
+    }
 }
